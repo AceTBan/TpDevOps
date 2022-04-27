7b97ef0f41930d88257aea8a021ec42e19a6380c
Johan Valero <valero_johan@hotmail.fr>
2022-04-27 16:56:45 +0200

    Update listeParis.php

--- controllers/listeParis.php
+++ controllers/listeParis.php
@@ -8,12 +8,26 @@ $matchs = new Matchs();
 $matchs->readAllJoueur();
 
 $allMatchs = $matchs->readAllJoueur();
-
+$listeJoueur = array();
 while($donnees = $allMatchs->fetch()){
-    $dateMatchs = $donnees['dateMatchs'];
-    $lieuMatchs = $donnees['lieuMatchs'];
     $nomJoueur = $donnees['nomJoueur'];
     $prenomJoueur = $donnees['prenomJoueur'];
-    include('../views/carteMatchs.php');
+
+    $nomJoueur1 = $donnees['nomJoueur'];
+    $prenomJoueur1 = $donnees['prenomJoueur'];
+    // array_push($listeJoueur, [$nomJoueur, $prenomJoueur]);
+    
+    $dateMatchs = $donnees['dateMatchs'];
+    $lieuMatchs = $donnees['lieuMatchs'];
+    // include('../views/carteMatchs.php');
+
+    echo $dateMatchs, '<br>';
+    echo $lieuMatchs, '<br>';
+    echo $nomJoueur , '<br>';
+    echo $prenomJoueur, '<br>';
+
+    // echo $nomjoueur1, '<br>';
+    // echo $listeJoueur[0][0], " " , $listeJoueur[0][1];  
+    // echo $listeJoueur[1][0], " " , $listeJoueur[1][1];
 }
 ?>
